6f167785159def64fd081a6da2efba2ae133a01f
Alyeev <markmarkul123@gmail.com>
2021-05-25 11:36:09 +0400

    Add Socket for Client

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 
 const app = express();
+// create server
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true
+  }  
+});;
 
 const rooms = new Map();
 
+//config web
 app.get("/rooms", (req, res) => {
-  console.log("hello");
   res.json(rooms);
 });
 
-app.listen(9999, (err) => {
+//config socket
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+});
+
+//config server
+server.listen(8888, (err) => {
   if (err) {
     throw Error(err);
   }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "./App.scss";
+import io from "socket.io-client";
 
-function App() {
-  return <div>Chat</div>;
-}
+const socket = io("http://localhost:8888");
+
+const App: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <div className="join-block">
+        <input type="text" placeholder="Room ID" />
+        <input type="text" placeholder="Your Name" />
+        <button className="btn btn-success">ENTER</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
