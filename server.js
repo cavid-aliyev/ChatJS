de92b2b83a8ba0d4275863f7b8a8529676e70486
Alyeev <markmarkul123@gmail.com>
2021-05-26 09:33:17 +0400

    Add axios

--- server.js
+++ server.js
@@ -17,13 +17,17 @@ app.get("/rooms", (req, res) => {
   res.json(rooms);
 });
 
+app.post("/rooms", (req, res) => {
+  console.log('Hello world');
+});
+
 //config socket
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
 });
 
 //config server
-server.listen(8888, (err) => {
+server.listen(9999, (err) => {
   if (err) {
     throw Error(err);
   }
--- src/Components/joinBlock.tsx
+++ src/Components/joinBlock.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-
+import axios from 'axios'
 
 const JoinBlock: React.FC = () => {
+  const [roomId, setRoomId] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+
+  const onEnter = () => {
+    if(!roomId || !userName){
+      return alert('Incorrect inputs')
+    }
+    axios.post('./rooms', {
+      roomId,
+      userName
+    })
+  }
+
   return (
     <div className="join-block">
-      <input type="text" placeholder="Room ID" />
-      <input type="text" placeholder="Your Name" />
-      <button className="btn btn-success">ENTER</button>
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(event) => setRoomId(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Your Name"
+        value={userName}
+        onChange={(event) => setUserName(event.target.value)}
+      />
+      <button onClick={onEnter} className="btn btn-success">ENTER</button>
     </div>
   );
 };
